ac5c8097fdcd19c879178670ef7136350d92eae0
Jamie Perkins <jamie.perkins@sailpoint.com>
2025-01-08 12:59:31 -0700

    "Unable to resolve bare specifier '@app/ng-home-root'"

--- single-spa/src/app-root-config.js
+++ single-spa/src/app-root-config.js
@@ -1,30 +1,38 @@
 import { registerApplication, start } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    import(
-      /* webpackIgnore: true */ // @ts-ignore-next
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
-
 // registerApplication({
-//   name: "@app/navbar",
+//   name: "@single-spa/welcome",
 //   app: () =>
 //     import(
 //       /* webpackIgnore: true */ // @ts-ignore-next
-//       "@app/navbar"
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
 //     ),
 //   activeWhen: ["/"],
 // });
 
-registerApplication(
-  "ng-home",
-  () => System.import("ng-home"),
-  (location) => location.pathname.startsWith("/")
-);
+// registerApplication({
+//   name: "ng-home",
+//   app: () =>
+//     import(
+//       /* webpackIgnore: true */ // @ts-ignore-next
+//       "ng-home"
+//     ),
+//   activeWhen: ["/"],
+// });
+
+registerApplication({
+  name: '@app/ng-home-root',
+  app: System.import('@app/ng-home-root').then(app => {
+    console.log('app', app);
+  }),
+  activeWhen: ['/'],
+});
+
+// registerApplication(
+//   "ng-home",
+//   () => System.import("ng-home"),
+//   (location) => location.pathname.startsWith("/")
+// );
 
 start({
   urlRerouteOnly: true,
